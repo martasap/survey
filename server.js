86a2a5fed19043cfc708410a44db457828b20499
Marta Sap <marta.magdalena.sap@gmail.com>
2015-06-14 21:00:27 +0200

    Form + some edits

--- server.js
+++ server.js
@@ -1,115 +1,95 @@
-// BASE SETUP
-// ======================================
-// CALL THE PACKAGES --------------------
-var express = require('express'); // call express
-var app = express(); // define our app using express
-var bodyParser = require('body-parser'); // get body-parser
-var morgan = require('morgan'); // used to see requests
-var mongoose = require('mongoose');
-var User = require('./app/models/user');
-var port = process.env.PORT || 8080; // set the port for our app
-// APP CONFIGURATION ---------------------
-// use body parser so we can grab information from POST requests
-app.use(bodyParser.urlencoded({ extended: true }));
+// wywołanie pakietów
+var express = require('express'); // wywołanie expressa - od tworzenia serwera
+var app = express(); // utworzenie serwera
+var bodyParser = require('body-parser'); // do parseowania
+var morgan = require('morgan'); // do logów w konsoli
+var mongoose = require('mongoose'); //mongoose - od bazy danych
+var User = require('./app/models/user'); //definiuje model aplikacji (pobranie schematu z user.js)
+var port = process.env.PORT || 8080; // ustawienie portu
+
+// konfiguracja aplikacji
+app.use(bodyParser.urlencoded({ extended: true }));// bodyParser - parsowanie żądań (żeby wyciągnąć informacje z żądań)
 app.use(bodyParser.json());
-// configure our app to handle CORS requests
-app.use(function(req, res, next) {
+app.use(function(req, res, next) {// ustawienie konfiguracji aplikacji, aby zapobiec błędom CORS (to pozwala dowolnej domenie na dostęp do API)
 res.setHeader('Access-Control-Allow-Origin', '*');
 res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
 res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
 next();
 });
-// log all requests to the console
-app.use(morgan('dev'));
-// connect to our database (hosted on modulus.io)
-mongoose.connect('mongodb://node:noder@novus.modulusmongo.net:27017/Iganiq8o');
-// ROUTES FOR OUR API
-// ======================================
-// basic route for the home page
-app.get('/', function(req, res) {
-res.send('Welcome to the home page!');
+app.use(morgan('dev'));// wypisuje logi żądań do konsoli
+mongoose.connect('mongodb://msap:msap@ds039231.mongolab.com:39231/survey');// łączenie się z bazą
+// ROUTING
+app.get('/', function(req, res) {// routing do strony głównej
+res.send('Homepage');
 });
-// get an instance of the express router
-var apiRouter = express.Router();
-// middleware to use for all requests
-apiRouter.use(function(req, res, next) {
-// do logging
+var apiRouter = express.Router();// routing do API
+apiRouter.use(function(req, res, next) {// funkcja do zalogowania wejścia do API
 console.log('Somebody just came to our app!');
-next(); // make sure we go to the next routes and don't stop here
+next(); // program szuka następnych funkcji, w które może wejść (nie kończy się)
 });
-// test route to make sure everything is working
-// accessed at GET http://localhost:8080/api
-apiRouter.get('/', function(req, res) {
-res.json({ message: 'hooray! welcome to our api!' });
+apiRouter.get('/', function(req, res) {//routing na (adres)/api	
+User.find(function(err, survey) {
+if (err) return res.send(err);
+res.json(survey);// zwraca ankietowanych
 });
-// on routes that end in /users
-// ----------------------------------------------------
-apiRouter.route('/users')
-// create a user (accessed at POST http://localhost:8080/users)
-.post(function(req, res) {
-var user = new User(); // create a new instance of the User model
-user.name = req.body.name; // set the users name (comes from the request)
-user.username = req.body.username; // set the users username (comes from the request)
-user.password = req.body.password; // set the users password (comes from the request)
+});
+
+apiRouter.route('/survey')// routing na (adres)/api/survey
+.post(function(req, res) {// tworzenie ankietowanego
+var user = new User(); 
+user.gender = req.body.gender;//wszystkie zmienne idą z żądania
+user.age = req.body.age; 
+user.fav_beer = req.body.fav_beer; 
+user.how_many = req.body.how_many;
+user.where = req.body.where;
+user.with_who = req.body.with_who;
 user.save(function(err) {
 if (err) {
-// duplicate entry
-if (err.code == 11000)
-return res.json({ success: false, message: 'A user with that username already exists. '});
+if (err.code == 11000)// błąd bazy przy duplikowaniu id
+return res.json({ success: false, message: 'Ankietowicz o tym id już istnieje. '});
 else
 return res.send(err);
 }
-// return a message
-res.json({ message: 'User created!' });
+res.json({ message: 'Ankieta wysłana!' });// zwraca odpowiedź
 });
 })
-// get all the users (accessed at GET http://localhost:8080/api/users)
-.get(function(req, res) {
-User.find(function(err, users) {
-if (err) return res.send(err);
-// return the users
-res.json(users);
+.get(function(req, res) {// odsyła do form.html
+res.sendFile(__dirname + '/form.html');
 });
-});
-// on routes that end in /users/:user_id
-// ----------------------------------------------------
-apiRouter.route('/users/:user_id')
-// get the user with that id
-.get(function(req, res) {
+apiRouter.route('/survey/:user_id')//routing do poszczególnej ankiety
+.get(function(req, res) {// pobiera ankietowanego z tym id
 User.findById(req.params.user_id, function(err, user) {
 if (err) return res.send(err);
-// return that user
-res.json(user);
+res.json(user);// zwraca go
 });
 })
-// update the user with this id
-.put(function(req, res) {
+
+.put(function(req, res) {// aktualizuje ankietowanego z danym id
 User.findById(req.params.user_id, function(err, user) {
 if (err) return res.send(err);
-// set the new user information if it exists in the request
-if (req.body.name) user.name = req.body.name;
-if (req.body.username) user.username = req.body.username;
-if (req.body.password) user.password = req.body.password;
-// save the user
-user.save(function(err) {
+
+if (req.body.gender) user.gender = req.body.gender;// ustawia nowe odpowiedzi
+if (req.body.age) user.age = req.body.age;
+if (req.body.fav_beer) user.fav_beer = req.body.fav_beer;
+if (req.body.how_many) user.how_many = req.body.how_many;
+if (req.body.where) user.where = req.body.where;
+if (req.body.with_who) user.with_who = req.body.with_who;
+user.save(function(err) {// zapisuje
 if (err) return res.send(err);
-// return a message
-res.json({ message: 'User updated!' });
+res.json({ message: 'Ankieta edytowana!' });// zwraca wiadomość
 });
 });
 })
-// delete the user with this id
-.delete(function(req, res) {
+
+.delete(function(req, res) {// usuwa ankietowanego
 User.remove({
 _id: req.params.user_id
 }, function(err, user) {
 if (err) return res.send(err);
-res.json({ message: 'Successfully deleted' });
+res.json({ message: 'Ankieta usunięta!' });
 });
 });
-// REGISTER OUR ROUTES -------------------------------
-app.use('/api', apiRouter);
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+
+app.use('/api', apiRouter);// zdefiniowanie ścieżki do API
+app.listen(port);//start serwera
+console.log('Nasłuchuję na porcie ' + port);
